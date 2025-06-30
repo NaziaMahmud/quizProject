e15ee74b825ecf9c99566eef85376afcec92a79f
Nazia Mahmud <nazia.mahmud70@gmail.com>
2025-06-30 14:43:42 -0400

    just formatting

--- index.js
+++ index.js
@@ -1,100 +1,114 @@
 //toggle quiz or create
 
-console.log('test')
-$(".creationButt").click(function(){
-    console.log('test');
-    $("#title").addClass("hidden");
-    $("#question-box").addClass("hidden");
-    $("#createQuestion").removeClass("hidden");});
+console.log("test");
+$(".creationButt").click(function () {
+  console.log("test");
+  $("#title").addClass("hidden");
+  $("#question-box").addClass("hidden");
+  $("#createQuestion").removeClass("hidden");
+});
 
-$("#quiz").click(function(){
-    console.log('test');
-    $("#createQuestion").addClass("hidden");
-    $("#title").removeClass("hidden");
-    $("#question-box").removeClass("hidden");
-    //display question
-    showQuestionNumber = 0
-    displayQuestion(questionList[0].question,questionList[0].option1, questionList[0].option2,questionList[0].option3,questionList[0].option4);
+$("#quiz").click(function () {
+  console.log("test");
+  $("#createQuestion").addClass("hidden");
+  $("#title").removeClass("hidden");
+  $("#question-box").removeClass("hidden");
+  //display question
 
-    
-    });
-$("#submitAnswer").click(function(){
-    //record user submission
-    userAnswer.push($("input[type='radio']:checked").val());
-    //go to next question
-    showQuestionNumber++;
-    
-    displayQuestion(questionList[showQuestionNumber].question,questionList[showQuestionNumber].option1, questionList[showQuestionNumber].option2,questionList[showQuestionNumber].option3,questionList[showQuestionNumber].option4);
-    if(showQuestionNumber===(questionList.length-1)){
-        //evaluate answers
-        let numberCorrect = 0;
-        for (k = 0; k<userAnswer.length;k++){
-            if (userAnswer[k] === answers[k]){
-                numberCorrect++;
-            }
-        }
-        $("#question-box").html(`You have ${numberCorrect} out of ${questionList.length} questions.`)
+  displayQuestion(
+    questionList[0].question,
+    questionList[0].option1,
+    questionList[0].option2,
+    questionList[0].option3,
+    questionList[0].option4
+  );
+  showQuestionNumber = 0;
+  userAnswer = [];
+});
+$("#submitAnswer").on("click", function (event) {
+  event.preventDefault();
+  //record user submission
+  userAnswer.push(
+    $("input[type='radio']:checked").closest("label").html().slice(20)
+  );
+  //go to next question
+  showQuestionNumber++;
+  if (showQuestionNumber < questionList.length) {
+    displayQuestion(
+      questionList[showQuestionNumber].question,
+      questionList[showQuestionNumber].option1,
+      questionList[showQuestionNumber].option2,
+      questionList[showQuestionNumber].option3,
+      questionList[showQuestionNumber].option4
+    );
+  }
+  if (showQuestionNumber === questionList.length) {
+    //evaluate answers
+    let numberCorrect = 0;
+    for (k = 0; k < userAnswer.length; k++) {
+      if (userAnswer[k] === answers[k]) {
+        numberCorrect++;
+      }
     }
+    $("#question").html(
+      `You have ${numberCorrect} out of ${questionList.length} questions.`
+    );
+  }
 });
 let showQuestionNumber = 0;
 let questionList = [];
 let userAnswer = [];
 let answers = [];
-function displayQuestion(Q,a1,a2,a3,a4){
-    //update question in html quiz section
-    $("#question").text(Q);
+function displayQuestion(Q, a1, a2, a3, a4) {
+  //update question in html quiz section
+  $("#question").text(Q);
 
-    let alist =[a1,a2,a3,a4];
-    let loopLength = alist.length
-    //randomly place options in labels in html quiz section
-    for (i = 0;i< loopLength;i++){
-        let rnum = Math.floor(Math.random()*alist.length);
-    
-        document.querySelectorAll("#answers label")[i].innerHTML= `<input type="radio">${alist[rnum]}`;
-        alist.splice(rnum,1);
-    }
-    
-    
-    
+  let alist = [a1, a2, a3, a4];
+  let loopLength = alist.length;
+  //randomly place options in labels in html quiz section
+  for (i = 0; i < loopLength; i++) {
+    let rnum = Math.floor(Math.random() * alist.length);
 
-
-
+    document.querySelectorAll("#answers label")[
+      i
+    ].innerHTML = `<input type="radio">${alist[rnum]}`;
+    alist.splice(rnum, 1);
+  }
 }
-function createQuestion(Q,a1,a2,a3,a4){//a1 has to always be the correct answer
-    return {
-        question:Q,
-        option1:a1,
-        option2:a2,
-        option3:a3,
-        option4:a4,
-        correct:a1,
-    }
+function createQuestion(Q, a1, a2, a3, a4) {
+  //a1 has to always be the correct answer
+  return {
+    question: Q,
+    option1: a1,
+    option2: a2,
+    option3: a3,
+    option4: a4,
+    correct: a1,
+  };
 }
 
-$("#submitQuestion").on("click",function(event){
-    event.preventDefault();//prevent reload
-    //grab info from from
-    let quest = document.getElementById("iformCreateQuestion").value;
-    console.log("value is"+ quest);
-    let op1 = document.querySelectorAll("#formCreateQuestion input")[1].value;
-    console.log("test"+op1);
-    let op2 = document.querySelectorAll("#formCreateQuestion input")[2].value;
-    console.log("test"+op2);
-    let op3 = document.querySelectorAll("#formCreateQuestion input")[3].value;
-    console.log("test"+op3);
-    let op4 = document.querySelectorAll("#formCreateQuestion input")[4].value;
-    console.log("test"+op4);
-    //create question object
-    let q1 = createQuestion(quest,op1,op2,op3,op4);
-    //add question object to question list
-    questionList.push(q1);
+$("#submitQuestion").on("click", function (event) {
+  event.preventDefault(); //prevent reload
+  //grab info from from
+  let quest = document.getElementById("iformCreateQuestion").value;
+  console.log("value is" + quest);
+  let op1 = document.querySelectorAll("#formCreateQuestion input")[0].value;
+  answers.push(op1);
+  console.log("test" + op1);
+  let op2 = document.querySelectorAll("#formCreateQuestion input")[1].value;
+  console.log("test" + op2);
+  let op3 = document.querySelectorAll("#formCreateQuestion input")[2].value;
+  console.log("test" + op3);
+  let op4 = document.querySelectorAll("#formCreateQuestion input")[3].value;
+  console.log("test" + op4);
+  //create question object
+  let q1 = createQuestion(quest, op1, op2, op3, op4);
+  //add question object to question list
+  questionList.push(q1);
 
-    console.log(questionList);
-    //resets form after submission
-    document.getElementById("formCreateQuestion").reset();
-})
+  console.log(questionList);
+  //resets form after submission
+  document.getElementById("formCreateQuestion").reset();
+});
 
-$("#submitAnswer").on("click",function(event){
-
-
-})
+$("#submitAnswer").on("click", function (event) {});
